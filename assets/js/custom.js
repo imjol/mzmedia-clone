c988cea2dd1c589f1648482bc3abd59a7bfd934a
mdrasel5 <mdrasel01333@gmail.com>
2024-05-21 11:46:32 +0600

    remake testimonail slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,5 @@
-// const swiper = new Swiper(".projectSlider", {
-//   speed: 400,
-//   spaceBetween: 100,
-//   slidesPerView: 4,
-//   loop: true,
-//   autoplay: {
-//     delay: 2000,
-//   },
-// });
 document.addEventListener("DOMContentLoaded", function () {
+  // Initialize Marquee
   const marquees = document.querySelectorAll(".rfm-marquee");
 
   marquees.forEach((marquee) => {
@@ -33,5 +25,47 @@ document.addEventListener("DOMContentLoaded", function () {
     marquee.parentElement.addEventListener("mouseout", () => {
       marquee.classList.remove("paused");
     });
+  });
+
+  // Testimonial Slider
+  const testimonials = document.querySelectorAll(".testimonial");
+  const nextButton = document.querySelector(".next-testimonial");
+  let currentIndex = 0;
+
+  function updateTestimonials() {
+    testimonials.forEach((testimonial, index) => {
+      if (index === currentIndex) {
+        testimonial.classList.add("opacity-100", "duration-500");
+        testimonial.classList.remove(
+          "opacity-0",
+          "max-sm:hidden",
+          "duration-300"
+        );
+      } else {
+        testimonial.classList.add("opacity-0", "max-sm:hidden", "duration-300");
+        testimonial.classList.remove("opacity-100", "duration-500");
+      }
+    });
+  }
+
+  const swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    loop: true,
+    on: {
+      init: function () {
+        currentIndex = this.realIndex; // 'this' refers to the Swiper instance
+        updateTestimonials();
+      },
+      slideChange: function () {
+        currentIndex = this.realIndex; // 'this' refers to the Swiper instance
+        updateTestimonials();
+      },
+    },
+  });
+
+  // Event listener for the next button
+  nextButton.addEventListener("click", function () {
+    swiper.slideNext();
   });
 });
